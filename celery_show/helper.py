e89cab0333ed18058b19aea77ebc63922966c013
amazedabhi <49566332+amazedabhi@users.noreply.github.com>
2021-01-27 11:50:59 +0530

    Update helper.py
    Updated message

--- celery_show/helper.py
+++ celery_show/helper.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 
 
 def send_mail_without_celery():
-    send_mail('CELERY WORKED YEAH','BEN STOKES',
+    send_mail('CELERY WORKED YEAH','hello',
                 'chintz.91@gmail.com',['srpvtltdngp@gmail.com'],
                 fail_silently=False)
     return None
